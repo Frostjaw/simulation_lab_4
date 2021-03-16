4b20f4aca1e8b404cf757517475a2a7b4cf2473b
user <user@DESKTOP-MLNQ3EN>
2021-03-17 00:41:47 +0700

    Fix usings

--- simulation_lab_4/Form1.cs
+++ simulation_lab_4/Form1.cs
@@ -1,14 +1,8 @@
 ﻿using MathNet.Numerics.Distributions;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Windows.Forms.DataVisualization.Charting;
 
 namespace simulation_lab_4
 {
